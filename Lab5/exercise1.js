a78b572a464eb40812b27dc3d9e8fc8d0a8304ef
Phyo Thinzar Aung <phyothinzar.ag92@gmail.com>
2023-06-08 17:57:54 -0500

    update lab5 with map, filter, reduce commit

--- Lab5/exercise1.js
+++ Lab5/exercise1.js
@@ -1,15 +1,19 @@
-function sum(arr){
-    let nums = arr.filter(function(element){
-        return element>20;
-    });
+// function sum(arr){
+//     let nums = arr.filter(function(element){
+//         return element>20;
+//     });
 
-    let sum = 0;
+//     let sum = 0;
 
-    for(let i=0; i<nums.length; i++){
-        sum += nums[i];
-    }
-    return sum;
+//     for(let i=0; i<nums.length; i++){
+//         sum += nums[i];
+//     }
+//     return sum;
     
+// }
+
+function sum(numArr){
+    return numArr.filter(elem => elem > 20).reduce((accum, elem) => accum + elem );
 }
 
 console.log(sum([40, 50, 10]));
--- Lab5/exercise2.js
+++ Lab5/exercise2.js
@@ -1,8 +1,12 @@
-const getNewArray = function(strArray){
-    let result = strArray.filter(function(element){
-        return element.length >= 5 && element.includes('a');
-    });
-    return result;
+// const getNewArray = function(strArray){
+//     let result = strArray.filter(function(element){
+//         return element.length >= 5 && element.includes('a');
+//     });
+//     return result;
+// }
+
+const getNewArray = function(strArr){
+    return strArr.filter(elem => elem.length > 5).filter(elem => elem.includes('a'));
 }
 
 console.log(getNewArray(["starry", "john", "jackin"]));
